61185f20f1881103c3d74e815939fb03658711a9
danny <onoratod@sas.upenn.edu>
2019-05-21 21:14:49 -0400

    Began with player meta data
    Added some code to harvest player meta data.

---
+++ utilities.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 21 19:52:48 2019
+
+@author: First Build
+"""
+
+def team_converter(team_id):
+    """
+    Converts a team's ID to their actual name.
+    """
+    if team_id == 1:
+        return "Arsenal"
+    elif team_id == 2:
+        return "Bournemouth"
+    elif team_id == 3:
+        return "Brighton"
+    elif team_id == 4:
+        return "Burnley"
+    elif team_id == 5:
+        return "Chelsea"
+    elif team_id == 6:
+        return "Crystal Palace"
+    elif team_id == 7:
+        return "Everton"
+    elif team_id == 8:
+        return "Huddersfield"
+    elif team_id == 9:
+        return "Leicester"
+    elif team_id == 10:
+        return "Liverpool"
+    elif team_id == 11:
+        return "Man City"
+    elif team_id == 12:
+        return "Man Utd"
+    elif team_id == 13:
+        return "Newcastle"
+    elif team_id == 14:
+        return "Southampton"
+    elif team_id == 15:
+        return "Stoke"
+    elif team_id == 16:
+        return "Swansea"
+    elif team_id == 17:
+        return "Spurs"
+    elif team_id == 18:
+        return "Watford"
+    return "West Brom"
+
+def position_converter(position):
+    """
+    Converts a player's `element_type` to their actual position.
+    """
+    if position == 1:
+        return "Goalkeeper"
+    elif position == 2:
+        return "Defender"
+    elif position == 3:
+        return "Midfielder"
+    else:
+        return "Forward"
+
