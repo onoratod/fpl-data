2fab54f11ed2706ed35e41704ecd31b18bdbdf98
danny <onoratod@sas.upenn.edu>
2019-05-26 15:14:46 -0400

    Added gameweek data

---
+++ harvest_meta.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 21 19:42:08 2019
+
+Scrape player summary data for 2018/2019
+"""
+
+import fpl, os
+import pandas as pd
+from utilities import team_converter, position_converter
+
+gitpath = "A:\\Github\\fpl-data\\data\\2018_2019"
+
+def get_player_meta(player):
+    # initialiaze meta dictionary
+    meta = {}
+    src = player._additional
+    
+    # store data
+    meta["player_id"] = src["id"]
+    meta["web_name"] = src["web_name"]
+    meta["team"] = team_converter(src["team"])
+    meta["first_name"] = src["first_name"]
+    meta["second_name"] = src["second_name"]
+    meta["cost_end"] = src["now_cost"]/10
+    meta["value_form"] = src["value_form"]
+    meta["value_season"] = src["value_season"]
+    meta["cost_change_start"] = src["cost_change_start"]/10
+    meta["cost_start"] = meta["cost_end"]-meta["cost_change_start"]
+    meta["dreamteam_count"] = src["dreamteam_count"]
+    meta["selected_by_percent"] = src["selected_by_percent"]
+    meta["total_points"] = src["total_points"]
+    meta["ppg"] = src["points_per_game"]
+    meta["minutes"] = src["minutes"]
+    meta["goals_scored"] = src["goals_scored"]
+    meta["assists"] = src["assists"]
+    meta["clean_sheets"] = src["clean_sheets"]
+    meta["goals_conceded"] = src["goals_conceded"]
+    meta["own_goals"] = src["own_goals"]
+    meta["penalties_saved"] = src["penalties_saved"]
+    meta["penalties_missed"] = src["penalties_missed"]            
+    meta["yellow_cards"] = src["yellow_cards"]
+    meta["red_cards"] = src["red_cards"]
+    meta["saves"] = src["saves"]
+    meta["bonus"] = src["bonus"]
+    meta["bps"] = src["bps"]
+    meta["influence"] = src["influence"]
+    meta["creativity"] = src["creativity"]
+    meta["threat"] = src["threat"]
+    meta["ict_index"] = src["ict_index"]
+    meta["ea_index"] = src["ea_index"]
+    meta["position"] = position_converter(src["element_type"])
+    
+    # store in list
+    for key,entry in meta.items():
+        meta[key] = [entry]
+    
+    df = pd.DataFrame.from_dict(meta, orient='index')
+    df.columns=['value']
+    df.index.name="entry"
+    return(df)
+
+# Loop over players, harvest meta data
+players = fpl.FPL().get_players()
+
+for ply in players:
+    meta_df = get_player_meta(ply)
+    ply_name = meta_df.query("entry=='web_name'").iloc[0]["value"]
+    
+    # Check whether player directory exists
+    player_path = os.path.join(gitpath, "Player", ply_name)
+    if not os.path.exists(player_path):
+        os.makedirs(player_path)
+        
+    meta_df.to_csv(os.path.join(player_path,"meta.csv"))
+
+    
+
+
---
+++ harvest_player_data.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 26 13:51:05 2019
+
+@author: First Build
+"""
+
+
+import fpl, os
+import pandas as pd
+import numpy as np
+from utilities import team_converter, position_converter
+
+gitpath = "A:\\Github\\fpl-data\\data\\2018_2019"
+
+
+data_shell = {
+                "kickoff_time_formatted" : [],
+                "team_h_score" : [],
+                "team_a_score" : [],
+                "was_home" : [],
+                "total_points" : [],
+                "value" : [],
+                "selected" : [],
+                "transfers_in" : [],
+                "transfers_out" : [],
+                "transfers_balance" : [],
+                "minutes" : [],
+                "goals_scored" : [],
+                "assists" : [],
+                "clean_sheets" : [],
+                "goals_conceded" : [],
+                "own_goals" : [],
+                "penalties_saved" : [],
+                "yellow_cards" : [],
+                "red_cards" : [],
+                "saves" : [],
+                "bonus" : [],
+                "bps" : [],
+                "influence" : [],
+                "creativity" : [],
+                "threat" : [],
+                "ict_index" : [],
+                "ea_index" : [],
+                "open_play_crosses" : [],
+                "big_chances_created" : [],
+                "clearances_blocks_interceptions" : [],
+                "recoveries" : [],
+                "key_passes" : [],
+                "tackles" : [],
+                "winning_goals" : [],
+                "attempted_passes" : [],
+                "completed_passes" : [],
+                "penalties_conceded" : [],
+                "big_chances_missed" : [],
+                "errors_leading_to_goal" : [],
+                "tackled" : [],
+                "offside" : [],
+                "target_missed" : [],
+                "fouls" : [],
+                "dribbles" : [],
+                "fixture" : [],
+                "opponent_team" : []
+            }
+
+def get_player_data(player):
+    # initialiaze data dictionary
+    data = data_shell
+    history = player.history
+    
+    for gw in range(1, len(history)):
+        for key, value in data.items():
+            data[key].append(history[gw][key])
+    
+    # Convert some data
+    data["opponent_team"] = [team_converter(opp) for opp in data["opponent_team"]]
+    data["value"] = [val/10 for val in data["value"]]
+    
+    # Balance the data on gameweeks
+    data_df = pd.DataFrame.from_dict(data)
+    data_df = data_df.append(pd.Series([np.nan]*len(data_df.columns), index=list(data_df.columns)), ignore_index=True)
+    data_df.index = data_df.index+1
+    
+    # Rename some columns
+    data_df.rename(index=str, columns= {"kickoff_time_formatted" : "kickoff_time",
+                               "team_h_score" : 'home_score',
+                               "team_a_score" : 'away_score'})
+    
+
+    return data_df
+
+
+
+# Loop over players, harvest meta data
+players = fpl.FPL().get_players()
+
+for ply in players:
+    data_df = get_player_data(ply)
+    ply_name = ply.name
+    
+    print("Harvesting... " + ply_name)
+    
+    # Check whether player directory exists
+    player_path = os.path.join(gitpath, "Player", ply_name)
+    if not os.path.exists(player_path):
+        os.makedirs(player_path)
+        
+    data_df.to_csv(os.path.join(player_path,"data.csv"))
+
+    
